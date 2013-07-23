12af942d25d05b2279705f156b901eccaee44fc9
zbysek <zbysek.nadenik@gmail.com>
2013-07-23 17:33:41 +0200

    asdf

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// NAV
+$(document).ready(function() {
+	$('.nav-inside ul li:last').addClass('last'); 
+
+	//Hide (Collapse) the toggle containers on load
+	$(".toggle_container").hide(); 
+
+	//Switch the "Open" and "Close" state per click then slide up/down (depending on open/close state)
+	$("h2.trigger").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow");
+		return false; //Prevent the browser jump to the link anchor
+	});	
+});
